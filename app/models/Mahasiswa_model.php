39a2eb8c4323a3fadff432dc783e9f57f98c08a7
TitoPutraaa <titobamulo13@gmail.com>
2025-09-27 20:04:15 +0800

    add mysql db to model, still put db conection in construct model

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,25 +1,40 @@
 <?php 
 
 class Mahasiswa_model {
-    private $mhs = [
-        [
-            "Nama" => "Tito Putra Bamulo",
-            "NIM" => "2415354011",
-            "Jurusan" => "Teknologi Informasi"
-        ],
-        [
-            "Nama" => "Sesilia Dehar",
-            "NIM" => "2415354033",
-            "Jurusan" => "Menejemen Bisnis"
-        ],
-        [
-            "Nama" => "Pramudya anantatoer",
-            "NIM" => "2415354099",
-            "Jurusan" => "Sastra Indonesia"
-        ],
-        ];
+    // private $mhs = [
+    //     [
+    //         "Nama" => "Tito Putra Bamulo",
+    //         "NIM" => "2415354011",
+    //         "Jurusan" => "Teknologi Informasi"
+    //     ],
+    //     [
+    //         "Nama" => "Sesilia Dehar",
+    //         "NIM" => "2415354033",
+    //         "Jurusan" => "Menejemen Bisnis"
+    //     ],
+    //     [
+    //         "Nama" => "Pramudya anantatoer",
+    //         "NIM" => "2415354099",
+    //         "Jurusan" => "Sastra Indonesia"
+    //     ],
+    //     ];
+
+    private $dbh;
+    private $stmt;
+
+    public function __construct() {
+        $dsn = 'mysql:host=localhost;dbname=MVC';
+
+        try {
+            $this->dbh = new PDO($dsn,'root', '');
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
 
         public function getAllMahasiswa() {
-            return $this->mhs;
+            $this->stmt = $this->dbh->prepare('SELECT * FROM Mahasiswa');
+            $this->stmt->execute();
+            return $this->stmt->fetchAll();
         }
     }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,20 +1,21 @@
 <div class="container mt-3">
-<table>
-        <thead>
-            <tr>
-                <th>Nama</th>
-                <th>NIM</th>
-                <th>Jurusan</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($data['mhs'] as $mhs) { ?>
+    <h1><?=$data['judul'] ?></h1>
+    <table>
+            <thead>
                 <tr>
-                    <td><?= $mhs['Nama']; ?></td>
-                    <td><?= $mhs['NIM']; ?></td>
-                    <td><?= $mhs['Nama']; ?></td>
+                    <th>Nama</th>
+                    <th>NIM</th>
+                    <th>Jurusan</th>
                 </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($data['mhs'] as $mhs) { ?>
+                    <tr>
+                        <td><?= $mhs['Nama']; ?></td>
+                        <td><?= $mhs['NIM']; ?></td>
+                        <td><?= $mhs['Jurusan']; ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
 </div>
